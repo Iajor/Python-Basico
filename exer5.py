23ba02e6826f389113be432a2b812008ac5f3154
André Rojai <118615132+Iajor@users.noreply.github.com>
2023-08-10 21:33:13 -0300

    Delete exer5.py

--- exer5.py
+++
@@ -1,5 +0,0 @@
-#5 - Faça um programa em linguagem Python que converta metros para centímetros.
-
-metro=int(input("Informe o valor em metros que deseja converter para centímetros: "))
-
-print(f'O valor em metro equivale a {metro*100} centímetros.')
