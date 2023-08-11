b4b23c96a9f7957ee5d180a6202b34d5a4db2bf2
André Rojai <118615132+Iajor@users.noreply.github.com>
2023-08-10 21:33:01 -0300

    Delete exer3.py

--- exer3.py
+++
@@ -1,9 +0,0 @@
-#3 - Refaça o programa anterior (refatorar) para que o programa recebe a distância e a velocidade média,
-#    mas calcule e exiba o tempo da viagem.
-
-
-distancia=int(input("Informe a distância a ser percorrida: "))
-velMedia=int(input("Informe a velocidade média na qual deseja viajar: "))
-
-print(f'A sua viagem levará {distancia/velMedia:.0f} horas')
-
